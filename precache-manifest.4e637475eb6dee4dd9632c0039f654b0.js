5aff9fbfa66c551d9c949af97e55e94d08ad7f2f
Anna Nefodova <ann.nefodova@gmail.com>
2020-07-28 10:17:48 +0300

    Updates

---
+++ precache-manifest.4e637475eb6dee4dd9632c0039f654b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ed2b95397380cd86ab21810201aefe",
+    "url": "/clone-trello/index.html"
+  },
+  {
+    "revision": "5f6cc64774ad88ed140b",
+    "url": "/clone-trello/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7a15915b9f10d2155002",
+    "url": "/clone-trello/static/css/main.97b5de26.chunk.css"
+  },
+  {
+    "revision": "5f6cc64774ad88ed140b",
+    "url": "/clone-trello/static/js/2.f0da6c21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clone-trello/static/js/2.f0da6c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a15915b9f10d2155002",
+    "url": "/clone-trello/static/js/main.33683544.chunk.js"
+  },
+  {
+    "revision": "c180fd82b490ae856b05",
+    "url": "/clone-trello/static/js/runtime-main.ef7f04fe.js"
+  }
+]);
